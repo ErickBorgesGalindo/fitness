68117e71d838eaa18b32602035d657e8778c343f
Erick Borges <erickborges@Ericks-MacBook-Air.local>
2022-11-28 20:32:45 -0600

    HeroSection, ButtonElements, and Background

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Video from '../../videos/video.mp4';
-import { HeroContainer, HeroBg, VideoBg } from './HeroSectionElements';
+import { Button } from '../ButtonElements';
+import { 
+  HeroContainer, 
+  HeroBg, 
+  VideoBg, 
+  HeroBtnWrapper, 
+  ArrowFoward, 
+  ArrowRight,
+  HeroH1,
+  HeroP,
+  HeroContent } from './HeroSectionElements';
+
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
   return (
     <HeroContainer>
-     <HeroBg>
-        <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
-     </HeroBg>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards your next payment.
+        </HeroP>
+
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} OnMouseLeave={onHover} primary="true" dark="true">
+            Get started {hover ? <ArrowFoward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }

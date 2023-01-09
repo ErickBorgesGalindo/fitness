f325c95338ded3e755b73dcbcbc35a40878e4dd7
Erick Borges <erickborges@Ericks-MacBook-Air.local>
2023-01-08 21:01:35 -0600

    First comit from smooth, just added more InfoSections

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -8,8 +8,42 @@ export const homeObjOne = {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
     buttonLabel: 'Comienza hoy',
     imgStart: false,
-    img: require('../../images/svg1.png'),
+    img: require('../../images/svg-1.svg'),
     alt: 'Gym',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Acceso ilimitado',
+    headline: 'Conoce nuestras ofertas y precios',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
+    buttonLabel: 'Conoce más',
+    imgStart: true,
+    img: require('../../images/svg-2.svg'),
+    alt: 'Gym2',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Gym',
+    headline: 'Manten tu cuerpo en forma y sano',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.',
+    buttonLabel: 'Comienza hoy',
+    imgStart: false,
+    img: require('../../images/svg-3.svg'),
+    alt: 'Gym3',
     dark: true,
     primary: true,
     darkText: false
